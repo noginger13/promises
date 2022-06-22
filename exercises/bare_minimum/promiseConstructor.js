3fe50ac2222c6f5e80e29c8679c5b0f4bc76eedb
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-22 11:30:42 -0400

    pass all tests for promise constructor

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -4,18 +4,28 @@
  * to the function invocation, while errors should be available in the `catch` block
  */
 
-var fs = require('fs');
-var request = require('needle');
 var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var request = Promise.promisifyAll(require('needle'));
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return fs.readFileAsync(filePath)
+    .then((data) => {
+      var text = (Buffer.from(data).toString());
+      var textArray = text.split('\n');
+      return textArray[0];
+    });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return request.getAsync(url)
+    .then ((res) => {
+      return res.statusCode;
+    });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
